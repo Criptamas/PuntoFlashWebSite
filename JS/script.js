fac52d5c26b71933cbc47847a78ef172ecc64f9a
Juan Rojas <abcdrojasgimenez@gmail.com>
2023-11-29 19:37:46 -0400

    Correccion final de errores de ortografia/ agg img

--- JS/script.js
+++ JS/script.js
@@ -29,3 +29,28 @@ for (let i = 0; i < navbarLinks.length; i++ ){
 //         header.classList.add('active');
 //     } else {header.classList.remove('active');}
 // })
+
+
+const deliveryBoy = document.querySelector("[data-delivery-boy]");
+
+let deliveryBoyMove = -80;
+let lastScrollPos = 0;
+
+window.addEventListener("scroll", function () {
+
+  let deliveryBoyTopPos = deliveryBoy.getBoundingClientRect().top;
+
+  if (deliveryBoyTopPos < 500 && deliveryBoyTopPos > -250) {
+    let activeScrollPos = window.scrollY;
+
+    if (lastScrollPos < activeScrollPos) {
+      deliveryBoyMove += 1;
+    } else {
+      deliveryBoyMove -= 1;
+    }
+
+    lastScrollPos = activeScrollPos;
+    deliveryBoy.style.transform = `translateX(${deliveryBoyMove}px)`;
+  }
+
+});
